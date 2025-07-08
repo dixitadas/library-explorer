faa998c43495b0bd9e06482799e75cdfa1dc0224
dixitadas <dixitadas2000@gmail.com>
2025-07-08 06:12:16 +0530

    book dao

---
+++ backend/src/main/java/com/dixitadas/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.dixitadas.library.repository;
+
+import com.dixitadas.library.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String book);
+}
