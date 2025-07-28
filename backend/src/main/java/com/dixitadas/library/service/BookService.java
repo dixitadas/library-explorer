9d370369050f8fbdbb74792199c5069eb45e7b91
dixitadas <dixitadas2000@gmail.com>
2025-07-29 03:01:13 +0530

    book service logic

--- backend/src/main/java/com/dixitadas/library/service/BookService.java
+++ backend/src/main/java/com/dixitadas/library/service/BookService.java
@@ -1,4 +1,27 @@
 package com.dixitadas.library.service;
 
+import com.dixitadas.library.model.Book;
+import com.dixitadas.library.repository.BookRepository;
+
+import java.util.List;
+
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public List<Book> searchBooks(String title) {
+        return bookRepository.findByTitleContainingIgnoreCase(title);
+    }
 }
