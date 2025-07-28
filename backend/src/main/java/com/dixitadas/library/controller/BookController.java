3abf5964939ef84b90e90702d50d1870625c2126
dixitadas <dixitadas2000@gmail.com>
2025-07-29 03:57:50 +0530

    update book controller

--- backend/src/main/java/com/dixitadas/library/controller/BookController.java
+++ backend/src/main/java/com/dixitadas/library/controller/BookController.java
@@ -2,9 +2,7 @@ package com.dixitadas.library.controller;
 
 import com.dixitadas.library.model.Book;
 import com.dixitadas.library.service.BookService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +14,11 @@ public class BookController {
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
     }
 
     @GetMapping
